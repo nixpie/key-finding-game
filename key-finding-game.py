a71ce6a6b0a1283d60f56ad5ea30a67bbd3a4765
nixpie <adriantrojanowski@protonmail.com>
2022-10-24 21:42:48 +0200

    second commit

--- key-finding-game.py
+++ key-finding-game.py
@@ -1,3 +1,4 @@
+from math import sqrt
 from random import randint
 
 GAME_WIDTH = 10
@@ -8,8 +9,13 @@ key_y = randint(0, GAME_HEIGHT)
 player_x = 0
 player_y = 0
 player_found_key = False
+steps = 0
+
+distance_before_move = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
+# print(key_x, key_y)
 
 while not player_found_key:
+    steps += 1
     print()
     print('Mozesz udać się w kierunkach określonych jako [W/A/S/D]: ')
 
@@ -46,7 +52,20 @@ while not player_found_key:
             print("Nie wiem, dokąd idziesz..")
             continue
 
+    if player_x == key_x and player_y == key_y:
+        print('Klucz jest Twój, mozesz iść otworzyć skarb!')
+        print(f'Wykonałaś/eś {steps} ruchów.')
+        quit()
+
+    distance_after_move = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
+
+    # print("before: ", distance_before_move)
+    # print("after: ", distance_after_move)
+    if distance_before_move > distance_after_move:
+        print('Cieplej!')
+    else:
+        print('Zimno!')
+
+    distance_before_move = distance_after_move
+
     print(player_x, player_y)
-
-
-
